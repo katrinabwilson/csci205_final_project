c468775b6d6d01452d5d83979f471e52b44b275e
Nolan Sauers <nls019@bucknell.edu>
2022-11-10 11:59:39 -0500

    NOLAN: implemented a LineGenerator test file

---
+++ src/test/java/CodeBashModel/LineGeneratorTest.java
@@ -0,0 +1,26 @@
+package CodeBashModel;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineGeneratorTest {
+
+    /** A LineGenerator object to use for testing */
+    private LineGenerator lineGenerator;
+
+    @BeforeEach
+    void setUp() {
+        lineGenerator = new LineGenerator();
+    }
+
+    @Test
+    void scanDocuments() throws IOException {
+        // Test to make sure some sentences are added
+        lineGenerator.scanDocuments();
+        assertTrue(lineGenerator.getSentenceList().size() > 0);
+    }
+}
