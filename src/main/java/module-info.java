af6508a293c80cdd74051d8817cdcbbd96091a24
Nolan Sauers <nls019@bucknell.edu>
2022-12-05 09:28:37 -0500

    NOLAN: small routine cleaning of files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,11 +6,6 @@ module csci205_final_project{
     requires javafx.controls;
     requires javafx.fxml;
 
-    exports TypingGame;
-    opens TypingGame to javafx.fxml;
-    exports TypingGame2;
-    opens TypingGame2 to javafx.fxml;
-
     exports CodeBash;
     opens CodeBash to javafx.fxml;
 
