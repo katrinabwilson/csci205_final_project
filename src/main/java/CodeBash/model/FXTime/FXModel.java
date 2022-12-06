7ade4c328564aa1fcc3ef77467f6b7a5ead1754d
Nahom Ayele <naa004@bucknell.edu>
2022-12-06 13:39:53 -0500

    Added Descriptions- Nahom

--- src/main/java/CodeBash/model/FXTime/FXModel.java
+++ src/main/java/CodeBash/model/FXTime/FXModel.java
@@ -8,17 +8,19 @@
  * Time: 11:19 AM
  *
  * Project: csci205_final_project
- * Package: TypingGame
+ * Package: FX Time
  * Class: FXModel
  *
- * Description:
+ * Description: This is a class that keeps our timer calculations separate.
+ * It serves as a vault that keeps our start times so that they won't get modified
+ * at another point in the program. We
  *
  * *****************************************/
 package CodeBash.model.FXTime;
 
 public class FXModel {
     /** This is the starting time of 15 seconds */
-    public Integer STARTTIME = 15;
+    private static Integer STARTTIME = 15;
     /** Setter method for the start time desired */
     public void setSTARTTIME(Integer STARTTIME) {
         this.STARTTIME = STARTTIME;
